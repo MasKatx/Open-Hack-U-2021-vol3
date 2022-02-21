f447083e617a8cf0a6fc4ea38a68a7c3c25c1ca6
ishida-0622 <ken.babuo@gmail.com>
2022-02-21 15:07:26 +0900

    検索画面の更新

--- public/src/main.js
+++ public/src/main.js
@@ -17,7 +17,7 @@ $("#main").on("click", async function () {
 const vue = new Vue({
     el: "#main",
     data: {
-        list: [],
+        bookSearchResult: [],
         title: "",
         author: "",
         publisher: "",
@@ -26,32 +26,34 @@ const vue = new Vue({
     },
     methods: {
         bookSearch: async function () {
-            this.list = [];
+            this.bookSearchResult = [];
             const sort = $("option:selected").val();
-            let url = `https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?format=json&sort=${sort}&applicationId=1088025467655525347&affiliateId=245eb4c3.2431bbf0.245eb4c4.d8af5e40`;
+            let url = `https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?format=json&sort=${sort}&applicationId=${fffun()}&affiliateId=245eb4c3.2431bbf0.245eb4c4.d8af5e40`;
             const urlTmp = url;
             url += this.title != "" ? `&title=${this.title}` : "";
             url += this.author != "" ? `&author=${this.author}` : "";
             url += this.publisher != "" ? `&publisherName=${this.publisher}` : "";
-            url += this.isbn != "" ? `&isbn=${this.isbn}` : "";
+            // url += this.isbn != "" ? `&isbn=${this.isbn}` : "";
             if (url === urlTmp){
+                alert("入力してください");
                 return;
             }
             const res = await fetch(url);
             const resJson = await res.json();
             this.flg = resJson.Items.length > 0;
             if (resJson.Items.length == 0) {
-                this.list.push({ title: "見つかりませんでした" });
+                this.bookSearchResult.push({ title: "見つかりませんでした" });
                 return;
             }
             for (let i = 0; i < resJson.Items.length; i++) {
-                this.list.push(
+                let item = resJson.Items[i].Item;
+                this.bookSearchResult.push(
                     {
-                        title: resJson.Items[i].Item.title,
-                        author: `著者 : ${resJson.Items[i].Item.author}`,
-                        img: resJson.Items[i].Item.mediumImageUrl,
-                        url: resJson.Items[i].Item.affiliateUrl,
-                        price: `価格 : ${resJson.Items[i].Item.itemPrice}円(税込)`,
+                        title: item.title.replace("　", " "),
+                        author: `著者 : ${item.author}`,
+                        img: item.mediumImageUrl,
+                        url: item.affiliateUrl,
+                        price: `価格 : ${item.itemPrice}円(税込)`,
                     }
                 );
             }
